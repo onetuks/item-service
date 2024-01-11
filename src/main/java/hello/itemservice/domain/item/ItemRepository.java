f947760ae0e8ed5abbf181157e7a60cabcf438a0
onetuks <onetuks@naver.com>
2024-01-11 12:59:48 +0900

    Chore: 섹션 3. 메시지, 국제화 초기 코드 구성

--- src/main/java/hello/itemservice/domain/item/ItemRepository.java
+++ src/main/java/hello/itemservice/domain/item/ItemRepository.java
@@ -1,17 +1,17 @@
 package hello.itemservice.domain.item;
 
+import org.springframework.stereotype.Repository;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import org.springframework.stereotype.Repository;
 
 @Repository
 public class ItemRepository {
 
-    // 동시성 문제를 고려하지 않은 컬렉션
-    private static final Map<Long, Item> store = new HashMap<>(); // ConcurrentHashMap
-    private static long sequence = 0L; // Atomic Long
+    private static final Map<Long, Item> store = new HashMap<>(); //static
+    private static long sequence = 0L; //static
 
     public Item save(Item item) {
         item.setId(++sequence);
@@ -32,10 +32,6 @@ public class ItemRepository {
         findItem.setItemName(updateParam.getItemName());
         findItem.setPrice(updateParam.getPrice());
         findItem.setQuantity(updateParam.getQuantity());
-        findItem.setOpen(updateParam.getOpen());
-        findItem.setRegions(updateParam.getRegions());
-        findItem.setItemType(updateParam.getItemType());
-        findItem.setDeliveryCode(updateParam.getDeliveryCode());
     }
 
     public void clearStore() {
