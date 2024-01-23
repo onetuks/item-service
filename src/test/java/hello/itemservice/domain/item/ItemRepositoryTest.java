4393cbfb3274a231bbd9204dd2b9bbaebe8d790e
onetuks <onetuks@naver.com>
2024-01-23 15:31:56 +0900

    Chore: MVC2 검증 소스 변경

--- src/test/java/hello/itemservice/domain/item/ItemRepositoryTest.java
+++ src/test/java/hello/itemservice/domain/item/ItemRepositoryTest.java
@@ -1,10 +1,11 @@
 package hello.itemservice.domain.item;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
-import java.util.List;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
 
 class ItemRepositoryTest {
 
@@ -17,52 +18,51 @@ class ItemRepositoryTest {
 
     @Test
     void save() {
-        // Given
+        //given
         Item item = new Item("itemA", 10000, 10);
 
-        // When
+        //when
         Item savedItem = itemRepository.save(item);
 
-        // Then
+        //then
         Item findItem = itemRepository.findById(item.getId());
         assertThat(findItem).isEqualTo(savedItem);
     }
 
     @Test
     void findAll() {
-        // Given
+        //given
         Item item1 = new Item("item1", 10000, 10);
-        Item item2 = new Item("item2", 10000, 20);
+        Item item2 = new Item("item2", 20000, 20);
 
         itemRepository.save(item1);
         itemRepository.save(item2);
 
-        // When
+        //when
         List<Item> result = itemRepository.findAll();
 
-        // Then
+        //then
         assertThat(result.size()).isEqualTo(2);
         assertThat(result).contains(item1, item2);
     }
 
     @Test
     void updateItem() {
-        // Given
+        //given
         Item item = new Item("item1", 10000, 10);
 
         Item savedItem = itemRepository.save(item);
         Long itemId = savedItem.getId();
 
-        // When
+        //when
         Item updateParam = new Item("item2", 20000, 30);
         itemRepository.update(itemId, updateParam);
 
-        // Then
         Item findItem = itemRepository.findById(itemId);
 
+        //then
         assertThat(findItem.getItemName()).isEqualTo(updateParam.getItemName());
         assertThat(findItem.getPrice()).isEqualTo(updateParam.getPrice());
         assertThat(findItem.getQuantity()).isEqualTo(updateParam.getQuantity());
     }
-
 }
