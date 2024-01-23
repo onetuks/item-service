76a3e11d9204f27511976535941814bf997aec09
onetuks <onetuks@naver.com>
2024-01-23 15:32:45 +0900

    Revert "Chore: MVC2 검증 소스 변경"
    This reverts commit 4393cbfb3274a231bbd9204dd2b9bbaebe8d790e.

--- src/test/java/hello/login/message/MessageSourceTest.java
+++ src/test/java/hello/login/message/MessageSourceTest.java
@@ -11,43 +11,44 @@ import org.springframework.context.MessageSource;
 import org.springframework.context.NoSuchMessageException;
 
 @SpringBootTest
-public class MessageSourceTest {
+class MessageSourceTest {
 
     @Autowired
-    MessageSource ms;
+    MessageSource messageSource;
 
     @Test
     void helloMessage() {
-        String result = ms.getMessage("hello", null, null);
+        String result = messageSource.getMessage("hello", null, null);
         assertThat(result).isEqualTo("안녕");
     }
 
     @Test
     void notFoundMessageCode() {
-        assertThatThrownBy(() -> ms.getMessage("no_code", null, null))
+        assertThatThrownBy(() -> messageSource.getMessage("no_code", null, null))
                 .isInstanceOf(NoSuchMessageException.class);
     }
 
     @Test
     void notFoundMessageCodeDefaultMessage() {
-        String result = ms.getMessage("no_code", null, "기본 메시지", null);
+        String result = messageSource.getMessage("no_code", null, "기본 메시지", null);
         assertThat(result).isEqualTo("기본 메시지");
     }
 
     @Test
     void argumentMessage() {
-        String result = ms.getMessage("hello.name", new Object[]{"Spring"}, null);
+        String result = messageSource.getMessage("hello.name", new Object[]{"Spring"}, null);
         assertThat(result).isEqualTo("안녕 Spring");
     }
 
     @Test
     void defaultLang() {
-        assertThat(ms.getMessage("hello", null, null)).isEqualTo("안녕");
-        assertThat(ms.getMessage("hello", null, Locale.KOREA)).isEqualTo("안녕");
+        assertThat(messageSource.getMessage("hello", null, null)).isEqualTo("안녕");
+        assertThat(messageSource.getMessage("hello", null, Locale.KOREA)).isEqualTo("안녕");
     }
 
     @Test
     void enLang() {
-        assertThat(ms.getMessage("hello", null, Locale.ENGLISH)).isEqualTo("hello");
+        assertThat(messageSource.getMessage("hello", null, Locale.ENGLISH)).isEqualTo("hello");
     }
+
 }
