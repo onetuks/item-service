3642f1c1b229915b2d65ab15e4143db320dde279
onetuks <onetuks@naver.com>
2024-01-27 22:11:31 +0900

    5. Bean Validation Form DTO 방식

---
+++ src/main/java/hello/itemservice/web/validation/form/ItemSaveForm.java
@@ -0,0 +1,23 @@
+package hello.itemservice.web.validation.form;
+
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+import org.hibernate.validator.constraints.Range;
+
+@Data
+public class ItemSaveForm {
+
+    @NotBlank
+    private String itemName;
+
+    @NotNull
+    @Range(min = 1_000, max = 1_000_000)
+    private Integer price;
+
+    @NotNull
+    @Max(value = 9_999)
+    private Integer quantity;
+
+}
